21b8e88bf14e8312cf341f83cd89f9019db659c8
sumin93 <suminfx@mail.ru>
2023-01-26 21:32:10 +0300

    remove asLiveData method

--- app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -1,19 +1,35 @@
 package com.sumin.coroutineflow.crypto_app
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 
 class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
 
-    val state: LiveData<State> = repository.getCurrencyList()
-        .filter { it.isNotEmpty() }
-        .map { State.Content(currencyList = it) as State }
-        .onStart { emit(State.Loading) }
-        .asLiveData()
+    private val _state = MutableLiveData<State>(State.Initial)
+    val state: LiveData<State> = _state
+
+    init {
+        loadData()
+    }
+
+    private fun loadData() {
+        repository.getCurrencyList()
+            .onStart {
+                val currentState = _state.value
+                if (currentState !is State.Content || currentState.currencyList.isEmpty()) {
+                    _state.value = State.Loading
+                }
+            }
+            .filter { it.isNotEmpty() }
+            .onEach { _state.value = State.Content(currencyList = it) }
+            .launchIn(viewModelScope)
+    }
 }
