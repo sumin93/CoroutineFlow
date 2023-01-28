ce9a6ceab0bd913424192ade0be81a81a0af0dfc
sumin93 <suminfx@mail.ru>
2023-01-29 00:47:56 +0300

    Add flow to view layer

--- app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoActivity.kt
+++ app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoActivity.kt
@@ -3,10 +3,17 @@ package com.sumin.coroutineflow.crypto_app
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.sumin.coroutineflow.databinding.ActivityCryptoBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.transform
+import kotlinx.coroutines.launch
 
 class CryptoActivity : AppCompatActivity() {
 
@@ -33,19 +40,28 @@ class CryptoActivity : AppCompatActivity() {
     }
 
     private fun observeViewModel() {
-        viewModel.state.observe(this) {
-            when (it) {
-                is State.Initial -> {
-                    binding.progressBarLoading.isVisible = false
+        lifecycleScope.launch {
+            viewModel.state
+                .transform {
+                    Log.d("CryptoViewModel", "Transform")
+                    delay(10_000)
+                    emit(it)
                 }
-                is State.Loading -> {
-                    binding.progressBarLoading.isVisible = true
+                .flowWithLifecycle(lifecycle, Lifecycle.State.RESUMED)
+                .collect {
+                    when (it) {
+                        is State.Initial -> {
+                            binding.progressBarLoading.isVisible = false
+                        }
+                        is State.Loading -> {
+                            binding.progressBarLoading.isVisible = true
+                        }
+                        is State.Content -> {
+                            binding.progressBarLoading.isVisible = false
+                            adapter.submitList(it.currencyList)
+                        }
+                    }
                 }
-                is State.Content -> {
-                    binding.progressBarLoading.isVisible = false
-                    adapter.submitList(it.currencyList)
-                }
-            }
         }
     }
 
--- app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -1,16 +1,14 @@
 package com.sumin.coroutineflow.crypto_app
 
 import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import kotlinx.coroutines.flow.*
 
 class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
 
-    val state: LiveData<State> = repository.getCurrencyList()
+    val state: Flow<State> = repository.getCurrencyList()
         .filter { it.isNotEmpty() }
         .map { State.Content(currencyList = it) as State }
         .onStart {
@@ -23,5 +21,4 @@ class CryptoViewModel : ViewModel() {
         .onCompletion {
             Log.d("CryptoViewModel", "Complete")
         }
-        .asLiveData()
 }
